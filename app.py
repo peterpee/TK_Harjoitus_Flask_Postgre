848d49d3a7ef6246979436e5f28b86f77bd0d6f9
peterpee <31952782+peterpee@users.noreply.github.com>
2023-10-19 19:46:14 +0300

    Lines added

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db =  SQLAlchemy(app)
+
 
 
